f4dad32b53e0a70ca87437d0ac80a1aa10865998
pmabres <pmabres@gmail.com>
2014-12-14 16:45:44 -0300

    test voice project

---
+++ testVoice/Assets/scripts/Globals.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Globals
+{
+    public static int CurrentLevel = 0;
+}
---
+++ testVoice/Assets/scripts/test.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	public Vector2 InitScale;
+	public Vector3 min;
+	public Vector3 max;
+	// Use this for initialization
+	void Start () {		
+        Debug.Log (gameObject.transform.localScale);
+		Debug.Log (gameObject.renderer.bounds.min);
+		Debug.Log (gameObject.renderer.bounds.max);
+		min = gameObject.renderer.bounds.min;
+		max = gameObject.renderer.bounds.max;
+		gameObject.renderer.bounds.SetMinMax (gameObject.renderer.bounds.min,max);
+		InitScale = new Vector2 (gameObject.transform.localScale.x*(Mathf.Abs(max.x - min.x)),
+		                         gameObject.transform.localScale.y*(Mathf.Abs(max.y - min.y)));
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.localScale = new Vector3(InitScale.x/Mathf.Abs(max.x - min.x),InitScale.y/Mathf.Abs(max.y - min.y),1);
+	}
+}
