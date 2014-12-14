bc011f39fc044bf6031d48b8a2e7a4694a8675a2
pmabres <pmabres@gmail.com>
2014-12-14 16:51:49 -0300

    .

--- testVoice/Assets/scripts/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public static class Constants  {
-    public const string DefaultJSONSettings = "{\"test\":\"esto es una prueba\"}";
-    public const string SettingsKey = "settings";
-}
--- testVoice/Assets/scripts/Globals.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public static class Globals
-{
-    public static int CurrentLevel = 0;
-}
